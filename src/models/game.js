ebbf13bb271ab39ea6beb652f96bb5196e18585e
Eduardo Araujo <eduardo.araujonf@gmail.com>
2023-06-18 20:16:25 -0300

    currentEquipsSchema Schema added

--- src/models/game.js
+++ src/models/game.js
@@ -19,12 +19,19 @@ const shopSchema = new mongoose.Schema({
 
 });
 
+const currentEquipsSchema = new mongoose.Schema({
+  sword: { type: any, required: true },
+  armor: { type: any, required: true  },
+  ring: { type: any, required: true  },
+  bracelet: { type: any, required: true },
+});
+
 const userSchema = new mongoose.Schema({
   _id: mongoose.Types.ObjectId,
   username: { type: String, required: true },
   experience: { type: Number, default: 0 },
   level: { type: Number, default: 0 },
-  current_equips: { type: [String], default: [] },
+  current_equips: { type: currentEquipsSchema, default: [] },
   shop_equips: { type: [shopSchema], default: [] },
   deck: { type: [cardSchema], default: [] },
   current_class: { type: String, default: 'none' }
